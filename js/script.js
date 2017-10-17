76026941a9f70823b560d0629ad2bd2106b921fa
Jotham Kimani <jothamkim79@gmail.com>
2017-10-17 12:07:48 +0300

    Modified the add() function.

--- js/script.js
+++ js/script.js
@@ -2,13 +2,15 @@
 // A function that adds two numbers
 
 
-var add = function(number1, number2){ return number1+ number2;
-};
+
 var number1 =parseInt(prompt("Enter a number:"));
 
 var number2 = parseInt(prompt("Enter another number:"));
 
-alert (add(number1, number2));
+var add = function(){ alert (number1+ number2)}
+
+
+add();
 
 
 
@@ -30,6 +32,9 @@ var number1 =parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
 
 alert (subtract(number1,number2));
+
+
+// A function that gets modulo of the first and second number
 
 
 var getModulo = function(number1, number2){ return number1%number2;
