d1e0db3804c2240a4dcaf1acc95a034181f0d0ff
Jotham Kimani <jothamkim79@gmail.com>
2017-10-17 12:24:51 +0300

    Modified the subtraction method.

--- js/script.js
+++ js/script.js
@@ -24,14 +24,15 @@ var multiply = function(){ alert(number1*number2)};
 
  multiply();
 
+//A function that subtracts two numbers
 
-var subtract= function(number1, number2){ return number1-number2;
-};
 var number1 =parseInt(prompt("Enter a number:"));
 
 var number2 = parseInt(prompt("Enter another number:"));
 
-alert (subtract(number1,number2));
+var subtract= function(){alert(number1-number2)};
+
+subtract();
 
 
 // A function that gets modulo of the first and second number
