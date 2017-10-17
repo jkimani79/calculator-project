e30b1d1015e7a05e56834228b6b864e843926ae9
Jotham Kimani <jothamkim79@gmail.com>
2017-10-17 09:54:19 +0300

    Added a getModulo function to scripts.js

--- js/script.js
+++ js/script.js
@@ -30,3 +30,12 @@ var number1 =parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
 
 alert (subtract(number1,number2));
+
+
+var getModulo = function(number1, number2){ return number1%number2;
+};
+var number1 =parseInt(prompt("Enter a number:"));
+
+var number2 = parseInt(prompt("Enter another number:"));
+
+alert (getModulo(number1, number2));
