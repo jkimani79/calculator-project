f1ca33f27ccde66f867d7dec98e467b6a96710b5
Jotham Kimani <jothamkim79@gmail.com>
2017-10-17 09:51:08 +0300

    Added a subtraction function to sctipt.js

--- js/script.js
+++ js/script.js
@@ -21,3 +21,12 @@ var number1 =parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
 
 alert (multiply(number1, number2));
+
+
+var subtract= function(number1, number2){ return number1-number2;
+};
+var number1 =parseInt(prompt("Enter a number:"));
+
+var number2 = parseInt(prompt("Enter another number:"));
+
+alert (subtract(number1,number2));
