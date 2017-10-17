f8f9bc5b0c60038dda525226e1a2f75ce08b846b
Jotham Kimani <jothamkim79@gmail.com>
2017-10-17 09:23:27 +0300

    Added a multiplication function.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+
+// A function that adds two numbers
+
+
 var add = function(number1, number2){ return number1+ number2;
 };
 var number1 =parseInt(prompt("Enter a number:"));
@@ -5,3 +9,15 @@ var number1 =parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
 
 alert (add(number1, number2));
+
+
+
+// A function that multiplies two numbers input from command prompt
+
+var multiply = function(number1, number2){ return number1*number2;
+};
+var number1 =parseInt(prompt("Enter a number:"));
+
+var number2 = parseInt(prompt("Enter another number:"));
+
+alert (multiply(number1, number2));
